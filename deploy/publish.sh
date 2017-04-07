65a561bb118d3b0215e8cf9e778fed75e3fd8a1f
Chris Carleton <Chris.Carleton@d2l.com>
2017-04-07 14:39:17 -0400

    Added debugging information to publish script.

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 # set AWS credentials
+echo "Setting AWS credentials..."
 aws configure set aws_access_key $2
 aws configure set aws_secret_access_key $3
 aws configure set default.region us-east-1
 
 # log into ECR
+echo "Logging into AWS ECR..."
 $(aws ecr get-login --region us-east-1)
+echo "Login succeeded."
 
 # tag and publish our latest stable build
+echo "Tagging and pushing docker image..."
 docker tag $4:latest $1.dkr.ecr.us-east-1.amazonaws.com/$4:latest
 docker push $1.dkr.ecr.us-east-1.amazonaws.com/$4:latest
+echo "Push succeeded."
