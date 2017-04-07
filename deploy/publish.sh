ac5e5d333e10e8a8cc08423d615820a7fb38f3da
Chris Carleton <Chris.Carleton@d2l.com>
2017-04-07 14:43:57 -0400

    Made correction to how AWS credentials get set.

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -2,7 +2,7 @@
 
 # set AWS credentials
 echo "Setting AWS credentials..."
-aws configure set aws_access_key $2
+aws configure set aws_access_key_id $2
 aws configure set aws_secret_access_key $3
 aws configure set default.region us-east-1
 
