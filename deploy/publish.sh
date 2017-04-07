ee42f953719beb072ce9587ef631d315e944e5b6
Chris Carleton <Chris.Carleton@d2l.com>
2017-04-07 14:27:49 -0400

    Trying alternate method of logging into ecr.

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -6,7 +6,7 @@ aws configure set aws_secret_access_key $3
 aws configure set default.region us-east-1
 
 # log into ECR
-aws ecr get-login --region us-east-1 | sh
+$(aws ecr get-login --region us-east-1)
 
 # tag and publish our latest stable build
 docker tag $4:latest $1.dkr.ecr.us-east-1.amazonaws.com/$4:latest
